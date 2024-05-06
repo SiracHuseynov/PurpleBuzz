10e5f1a22d8355abfc2431f0bc211d89ee777532
SiracHuseynov <tu6uvrrlv@code.edu.az>
2024-05-07 02:08:15 +0400

    add

---
+++ PurpleBuzz/Controllers/WorkController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PurpleBuzz.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WorkSingle()
+        {
+            return View();
+        }
+
+    }
+}
